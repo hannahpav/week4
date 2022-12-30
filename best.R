75646c3793e5c97dedc5fd82e4a03573defc238f
hannahpav <hannah.pavlovich@gmail.com>
2022-12-30 20:24:00 +0100

    I get a warning still with my coercion creating NA, but I really don't care

--- best.R
+++ best.R
@@ -1,33 +1,55 @@
 best <- function(state, outcome) {
   ## Read outcome data
-  outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-  ## Check that state and outcome are valid
-  
-  ## Return hospital name in that state with lowest 30-day death
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
+  ##First I want to convert those outcome measures to the easily 
+  ## extractable values in the table
   
   if(outcome == "heart attack") {
     death <- 11
-    }
-    else(outcome == "heart failure") {
-      death <- 17
-      }
-      else(outcome == "pneumonia") {
-        death <- 24}
-}
+  }
+  else if(outcome == "heart failure") {
+    death <- 17
+  }
+  else if(outcome == "pneumonia") {
+    death <- 23}
+  
+  ## Check that state and outcome are valid
+  
+  statenames <- unique(data$State)
+  
+ statetruth <- state %in% statenames
+ outcometruth <- death == 11 || death == 17 || death == 23
+ 
+ if(statetruth == FALSE){
+   stop("invalid state")
+ }
+ if(outcometruth == FALSE){
+   stop("invalid outcome")
+ }
 
+ if(outcometruth == FALSE || statetruth == FALSE){
+   
+ }
+  
+    ## Return hospital name in that state with lowest 30-day death
 
-littleframe <- data.frame(outcome[ , "Hospital.Name"], outcome[,death], outcome[ , "State"] )
-allstates <- split(littleframe, outcome$State)
+ #Create a frame just with the three columns we need
+littleframe <- data.frame(data[ , "Hospital.Name"], data[,death], data[ , "State"] )
+#split the frame into three frames
+allstates <- split(littleframe, data$State)
 
+#create a matrix of just our state
 statedata <- allstates[[state]]
 
+#rename the columns becasue it's easier
 colnames(statedata) <- c("hospital", "death", "state")
-statedata <- 
 
+#sort the data from low to high
+numberdeath <- as.numeric(statedata$death)
+newframe <- data.frame(statedata$hospital, numberdeath)
+newframe <- na.omit(newframe)
+sortedstate <- newframe[order(newframe$numberdeath),]
 
-  "heart attack"
-  "heart failue"
-  "pnemonia"
-  only <- data.frame(outcome$Hospital.Name, outcome$outcome, outcome$state)
-  ## rate
+print(sortedstate[1, 1])
 }
